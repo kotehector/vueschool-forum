3c1482bd6d43c9e061e8a27544ac07b193fa5031
Héctor Menduíña <kotehector@gmail.com>
2018-05-06 14:44:20 +0200

    Manejo de Páginas No Encontradas en la App.
    - Crear Componente Página para las páginas no encontradas.
    - CSS Scoped (solo afecta a este componente)

---
+++ src/pages/PageNotFound.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="col-full">
+    <h1>Not Found</h1>
+    <p>
+      Ooops, we couldn't find that your are looking for. Why don't you
+      <router-link :to="{ name: 'Home'}">
+        go home instead?
+      </router-link>
+    </p>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'PageNotFound'
+}
+</script>
+
+<style scoped>
+  h1 {
+    font-size: 6rem
+  }
+  p {
+    font-size: 2rem;
+    font-weight: 100;
+  }
+  div {
+    text-align: center;
+  }
+</style>
